189608efd4c8548634415e56634cd826853358a2
diippidm <habibullaerasyl07@gmail.com>
2024-10-19 15:09:07 +0500

    app.py

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
-"""app.py"""
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
 from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler, MessageHandler, filters
 from credentials import BOT_TOKEN, BOT_USERNAME
 import json
 
+# Инициализация объекта application
+application = ApplicationBuilder().token(BOT_TOKEN).build()
+
 async def launch_web_ui(update: Update, callback: CallbackContext):
-    # display our web-app!
     kb = [
-        [KeyboardButton(
-            "Show me my Web-App!", 
-           web_app=WebAppInfo("https://diippdim.github.io/") # obviously, set yours here.
-        )]
+        [KeyboardButton("Show me my Web-App!", web_app=WebAppInfo("https://diippdim.github.io/"))]
     ]
     await update.message.reply_text("Let's do this...", reply_markup=ReplyKeyboardMarkup(kb))
 
-
-if __name__ == '__main__':
-    # when we run the script we want to first create the bot from the token:
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
-
-    # and let's set a command listener for /start to trigger our Web UI
+if __name__ == "__main__":
     application.add_handler(CommandHandler('start', launch_web_ui))
-
-    # and send the bot on its way!
     print(f"Your bot is listening! Navigate to http://t.me/{BOT_USERNAME} to interact with it!")
-    application.run_polling()
+    application.run_polling()  # Метод для запуска приложения без необходимости вручную запускать start()
